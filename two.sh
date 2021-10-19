ed8841284184f7efe7b139bb353ea7a4a2581701
TwilyHoney <75704411+TwilyHoney@users.noreply.github.com>
2021-10-20 03:34:28 +0700

    Update two.sh
    Make sure to actually mention where the text file is ._.

--- two.sh
+++ two.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-rm /root/result.txt
-bash /root/virtual-pony/one.sh >> result.txt
-cat /root/result.txt | s-nail -s 'Hey did i have an update' -r moon@pinkiehoney.eu.org rarihoney@gmail.com
+rm /root/virtual-pony/result.txt
+bash /root/virtual-pony/one.sh >> /root/virtual-pony/result.txt
+cat /root/virtual-pony/result.txt | s-nail -s 'Hey did i have an update' -r moon@pinkiehoney.eu.org rarihoney@gmail.com
