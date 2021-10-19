24e139e4d268beb8fe1e9c5b4dc5e6888a9bd1a6
TwilyHoney <75704411+TwilyHoney@users.noreply.github.com>
2021-10-19 18:55:36 +0700

    Update cook.sh
    Domain change to glowup subdomain because my usual domain got rate-limited, and predefined nginx config file mentioned earlier. Ready for makeovers? ^^

--- cook.sh
+++ cook.sh
@@ -8,14 +8,14 @@ apt install nodejs
 wget https://github.com/thelounge/thelounge/releases/download/v4.2.0/thelounge_4.2.0_all.deb
 apt install ./thelounge_4.2.0_all.deb
 apt install nginx
-nano /etc/nginx/sites-available/pinkiehoney.eu.org
-ln -s /etc/nginx/sites-available/pinkiehoney.eu.org /etc/nginx/sites-enabled/pinkiehoney.eu.org
+mv /root/virtual-pony/glowup.izzymoonbow.eu.org /etc/nginx/sites-available/glowup.izzymoonbow.eu.org
+ln -s /etc/nginx/sites-available/glowup.izzymoonbow.eu.org /etc/nginx/sites-enabled/glowup.izzymoonbow.eu.org
 nginx -t
 systemctl restart nginx
 sudo -u thelounge thelounge add tiffany
 sudo -u thelounge thelounge install thelounge-theme-mininapse
 apt install certbot python3-certbot-nginx
-certbot --nginx -d pinkiehoney.eu.org
+certbot --nginx -d glowup.izzymoonbow.eu.org
 sudo DEBIAN_PRIORITY=low apt install postfix
 sudo postconf -e 'home_mailbox= Maildir/'
 sudo postconf -e 'virtual_alias_maps= hash:/etc/postfix/virtual'
@@ -23,13 +23,12 @@ sudo nano /etc/postfix/virtual
 sudo postmap /etc/postfix/virtual
 sudo systemctl restart postfix
 echo 'export MAIL=~/Maildir' | sudo tee -a /etc/bash.bashrc | sudo tee -a /etc/profile.d/mail.sh
-# Command below may fail if you're not using standard bash. Redo the source and init command
-# If it fails. And don't forget to check the structure.
+# Command below may fail. Structure is created after sending first email anyway.
 source /etc/profile.d/mail.sh
 sudo apt install s-nail
 sudo nano /etc/s-nail.rc
 echo 'init' | s-nail -s 'init' -Snorecord root
-ls -R ~/Maildir
 nano ~/content
 cat ~/content | s-nail -s 'Hi new friend' -r moon@pinkiehoney.eu.org rarihoney@gmail.com
+ls -R ~/Maildir
 echo "It's cooked!"
