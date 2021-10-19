662c4ad018e3cf78a942b1d2ffabd4ad2976c1be
TwilyHoney <75704411+TwilyHoney@users.noreply.github.com>
2021-10-19 19:03:48 +0700

    Create one.sh
    Main code for autoupdate script. Don't suggest me to use unattended-upgrades.

---
+++ one.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+echo "PonyUp script. Copyright 2021 CanterLogic Inc."
+echo
+echo "Updating package list..."
+apt-get update
+echo
+echo "Upgrading packages if any..."
+apt-get -y upgrade
+echo
+echo "Removing unused packages if any..."
+apt-get -y autoremove
+if [ -f /var/run/reboot-required ]; then
+  echo 'Hey please head over to VPS dashboard and click restart'
+fi
