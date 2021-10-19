212f62e9ed3de502e9d0c45d3db99e38bfaf21a8
TwilyHoney <75704411+TwilyHoney@users.noreply.github.com>
2021-10-19 19:06:01 +0700

    Create two.sh
    Actual code for stuff

---
+++ two.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+rm /root/result.txt
+bash /root/virtual-pony/one.sh >> result.txt
+cat /root/result.txt | s-nail -s 'Hey did i have an update' -r moon@pinkiehoney.eu.org rarihoney@gmail.com
