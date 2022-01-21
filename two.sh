0946515d82b52076a3956a776ffcae17848a7186
TwilyHoney <75704411+TwilyHoney@users.noreply.github.com>
2022-01-21 12:08:07 +0700

    Update two.sh
    Hello 123

--- two.sh
+++ two.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 rm /root/result.txt
 bash /root/one.sh >> /root/result.txt
-cat /root/result.txt | s-nail -s 'Hey did i have an update' -r moon@static.126.144.119.168.clients.your-server.de rarihoney@gmail.com
+cat /root/result.txt | msmtp rarihoney@gmail.com
