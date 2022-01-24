67fa171476518e8513edc4f167167b50108b368e
TwilyHoney <75704411+TwilyHoney@users.noreply.github.com>
2022-01-25 05:29:32 +0700

    Update two.sh
    Add some comments

--- two.sh
+++ two.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
-rm /root/result.txt
-bash /root/one.sh >> /root/result.txt
-cat /root/result.txt | s-nail -s "hey did i have an update" -r moon@unassigned.l1servers.com thirdpony@outlook.com
+# On Server with control panel, use "/home/domain_without_tld_part/public_html". Otherwise, use "/var/www/your_domain".
+rm /home/lauraorchid/public_html/result.txt
+# Make sure to output to your web root.
+bash /root/one.sh >> /home/lauraorchid/public_html/result.txt
+# Afterwards, open https://your_domain/result.txt
